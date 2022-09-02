779b63e829d19b3884b6fee0a340f5bdc0874548
sinacd <sina.saeedipoor1@gmail.com>
2022-09-02 15:15:15 +0430

    netlify ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDX5NBk9DOBvuoO2dOxJX77Ffl5i1sr5EE",
+  authDomain: "fir-demo-6a63e.firebaseapp.com",
+  databaseURL: "https://fir-demo-6a63e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fir-demo-6a63e",
+  storageBucket: "fir-demo-6a63e.appspot.com",
+  messagingSenderId: "877937243592",
+  appId: "1:877937243592:web:ed81dfdd0d46d10d4f7cfd",
+  measurementId: "G-ZMZZ717LY6"
+  }
 };
